954c14c30397d5665738feb38cdd0ea67d419cac
nandan8983 <103305922+nandan8983@users.noreply.github.com>
2025-05-29 22:21:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const makeChange = (c) => {
   // your name here
+	let change = {};
+
+	amount=c;
+  change.q = Math.floor(amount / 25);
+  amount %= 25;
+
+  change.d = Math.floor(amount / 10);
+  amount %= 10;
+
+  change.n = Math.floor(amount / 5);
+  amount %= 5;
+
+  change.p = amount;
+
+  return change;
+	
 };
 
 // Do not the change the code below
